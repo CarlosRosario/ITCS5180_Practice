08b3d3abe5558c052ce7568a8d2ec0e4fcbcf9ee
Carlos Rosario <carlos.rosario1990@gmail.com>
2016-01-10 15:20:12 -0500

    initial android stuff

---
+++ MySecondAndroidApp/app/src/main/java/com/crosario/mysecondandroidapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.crosario.mysecondandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        Log.i("INFO", "Button has been clicked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ MyThirdAndroidApp/app/src/main/java/com/crosario/mythirdandroidapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.crosario.mythirdandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends asdfasdf {
+
+    public void clickFunction(View view){
+        EditText usernameEditText = (EditText)findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+
+        Log.i("INFO", "username=" + usernameEditText.getText().toString() + " password=" + passwordEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
