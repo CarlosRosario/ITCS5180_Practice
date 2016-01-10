33d4a35e6fa772bfa24c2925f23fa84643f07a37
Carlos Rosario <Carlos Rosario>
2016-01-10 16:53:47 -0500

    alicia is my friend..

--- MyThirdAndroidApp/app/src/main/java/com/crosario/mythirdandroidapp/MainActivity.java
+++ MyThirdAndroidApp/app/src/main/java/com/crosario/mythirdandroidapp/MainActivity.java
@@ -6,12 +6,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends asdfasdf {
+public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
         EditText usernameEditText = (EditText)findViewById(R.id.usernameEditText);
         EditText passwordEditText = (EditText)findViewById(R.id.passwordEditText);
-
+        // added a comment
+        // adding a second mfing comment
         Log.i("INFO", "username=" + usernameEditText.getText().toString() + " password=" + passwordEditText.getText().toString());
     }
 
